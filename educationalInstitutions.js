b9336e33221305b2c28eb826ae000de013c05657
barsha bhandari <chettriame@gmail.com>
2024-03-05 19:18:59 +0545

    okayy

---
+++ educationalInstitutions.js
@@ -0,0 +1,321 @@
+export const institutionsUtils = {
+  albums: [
+    {
+      thumbnail:
+        "https://res.cloudinary.com/dcgrv6shk/image/upload/v1705222929/Eduroclass/Uploads/EducationInstitutions/kcjt7pxrmuecb70byky0.jpg",
+    },
+    {
+      thumbnail:
+        "https://upload.wikimedia.org/wikipedia/commons/f/f4/Nepal_Medical_College_%26_Teaching_Hospita%2C_Attarkhel%2C_Jorpati%2C_Kathmandu.jpg",
+    },
+  ],
+  programs: [
+    {
+      name: "Computer Science",
+    },
+    { name: "Electrical Engineering" },
+    { name: "Business Administration" },
+    { name: "Environmental Science" },
+    { name: "Software Engineering" },
+    { name: "Network Science" },
+    { name: "Computer Applications" },
+    { name: "Game Science" },
+
+
+
+    // Add more programs...
+  ],
+  faculty: [
+    {
+      name: "Prof.Arjun Bhandari",
+      qualification: "Ph.D. in Physics",
+      experience: 10,
+    },
+    // Add more faculty members...
+    {
+      name: "Dr. Shree Ram Baidya",
+      qualification: "Ph.D. in Computer Science",
+      experience: 15,
+    },
+    {
+      name: "Prof. Almala Gurung",
+      qualification: "Ph.D. in Chemistry",
+      experience: 8,
+    },
+    {
+      name: "Dr. Shruti shrestha",
+      qualification: "Ph.D. in Mathematics",
+      experience: 12,
+    },
+    {
+      name: "Dr. ",
+      qualification: "Ph.D. in Neurology",
+      experience: 8,
+    },
+    {
+      name: "Dr. Krishna Maharjan",
+      qualification: "Ph.D. in History",
+      experience: 9,
+    },
+
+  ],
+  extraActivities: [
+    {
+      name: "Robotics Club",
+    },
+    // Add more extracurricular activities...
+    { name: "Debating Society" },
+    { name: "Art and Music Club" },
+    { name: "Environmental Awareness Group" },
+    // Add more extracurricular activities...
+    { name: "Community Service Club" },
+    { name: "Entrepreneurship and Innovation Hub" },
+    { name: "Literary Magazine Editorial Team" },
+    { name: "Photography and Film Club" },
+    { name: "Chess Club" },
+    { name: "Health and Wellness Committee" },
+  ],
+  cost: [
+    {
+      tuition: 15000,
+      fees: 2000,
+      livingExpenses: 10000,
+    },
+    // Add more cost details...
+    {
+      tuition: 12000,
+      fees: 1800,
+      livingExpenses: 9000,
+    },
+    {
+      tuition: 17000,
+      fees: 2500,
+      livingExpenses: 12000,
+    },
+    {
+      tuition: 14000,
+      fees: 2200,
+      livingExpenses: 9500,
+    },
+  ],
+  alumniSuccess: [
+    {
+      description: "Successful entrepreneur in the tech industry.",
+    },
+    {
+      description: "Influential political leader and advocate for social justice.",
+    },
+    {
+      description: "Award-winning author and literary critic.",
+    },
+    {
+      description: "Leading expert in artificial intelligence and machine learning.",
+    },
+    {
+      description: "Renowned medical researcher and healthcare innovator.",
+    },
+    {
+      description: "Successful finance and investment strategist.",
+    },
+    
+    // Add more alumni success stories...
+    {
+      description: "Renowned scientist in the field of biology.",
+    },
+    {
+      description: "Accomplished artist and designer.",
+    },
+    {
+      description: "Pioneer in sustainable energy solutions.",
+    },
+  ],
+  infrastructure: [
+    {
+      name: "Main Building",
+      classrooms: 20,
+      library: 1,
+      laboratories: 23,
+      sportsFacilities: 34,
+    },
+    {
+      name: "DDBuilding",
+      classrooms: 50,
+      library: 1,
+      laboratories: 23,
+      sportsFacilities: 34,
+    },
+    // Add 3 more infrastructure details...
+    {
+      name: "Science Block",
+      classrooms: 30,
+      library: 1,
+      laboratories: 23,
+      sportsFacilities: 43,
+    },
+    {
+      name: "Arts Building",
+      classrooms: 40,
+      library: 1,
+      laboratories: 12,
+      sportsFacilities: 4,
+    },
+    {
+      name: "Engineering Wing",
+      classrooms: 25,
+      library: 1,
+      laboratories: 6,
+      sportsFacilities: 5,
+    },
+    {
+      name: "Digital Media Studio",
+      classrooms: 12,
+      library: 1,
+      laboratories: 10,
+      sportsFacilities: 3,
+    },
+    {
+      name: "Language Learning Center",
+      classrooms: 15,
+      library: 1,
+      laboratories: 5,
+      sportsFacilities: 2,
+    },
+  ],
+};
+export const institutionsData = [
+  {
+    name: "Elite College of Engineering",
+    albumIds: [1, 2],
+    programsIds: [1, 2],
+    facultyIds: [1, 2],
+    extraActivitiesIds: [1, 2],
+    costIds: [1, 2],
+    alumniSuccessIds: [1, 2],
+    insfrastructureIds: [1, 2],
+    thumbnail:
+      "https://upload.wikimedia.org/wikipedia/commons/f/f4/Nepal_Medical_College_%26_Teaching_Hospita%2C_Attarkhel%2C_Jorpati%2C_Kathmandu.jpg",
+    description: "Leading institution for engineering studies",
+    address: "Chabahil,Kathmandu",
+    type: "College",
+    category: "Engineering",
+    accreditation_status: "Accredited",
+    principal_name: "Dr. Shijan Shah",
+    principal_email: "elitecollege@45.edu",
+    principal_phone_number: "+977 98125252522",
+    files: "https://elitecollege.edu/files/brochure.pdf",
+    ownerId: 1,
+  },
+  {
+    name: "Bloomfield High School",
+    albumIds: [1, 2],
+    programsIds: [1, 2],
+    facultyIds: [1, 2],
+    extraActivitiesIds: [1, 2],
+    costIds: [1, 2],
+    alumniSuccessIds: [1, 2],
+    insfrastructureIds: [1, 2],
+    thumbnail:
+      "https://patch.com/img/cdn20/users/22844250/20210427/094626/styles/patch_image/public/bloomfield-highschool-googlemaps___27094435889.jpg",
+    description: "Excellence in secondary education",
+    address: "Birauta-17,Pokhara",
+    type: "School",
+    category: "Secondary Education",
+    accreditation_status: "Accredited",
+    principal_name: "Mrs. Shruti jha",
+    principal_email: "ly@bloomfieldhigh.edu",
+    principal_phone_number: "+98765432100",
+    files: "https://bloomfieldhigh.edu/documents/admission_form.pdf",
+    ownerId: 2,
+  },
+  {
+    name: "Global Arts Institute",
+    albumIds: [1, 2],
+    programsIds: [1, 2],
+    facultyIds: [1, 2],
+    extraActivitiesIds: [1, 2],
+    costIds: [1, 2],
+    alumniSuccessIds: [1, 2],
+    insfrastructureIds: [1, 2],
+
+    thumbnail:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGDneMiWOSVINeo8AoOTY9st--M2cQDTEr35n8R0XRDDj9aL3wR2j_qj8Zh9nVGSvkiE8&usqp=CAU",
+    description: "Nurturing creativity and artistic talent",
+    address: "Kalanki,Kathmandu",
+    type: "Institute",
+    category: "Arts",
+    accreditation_status: "Accredited",
+    principal_name: "Prof.Krishna Prasad Oli",
+    principal_email: "ol@globalarts.edu",
+    principal_phone_number: "+9822334455",
+    files: "https://globalarts.edu/files/prospectus.pdf",
+    ownerId: 3,
+  },
+  {
+    name: "Innovative Academy",
+    albumIds: [1, 2],
+    programsIds: [1, 2],
+    facultyIds: [1, 2],
+    extraActivitiesIds: [1, 2],
+    costIds: [1, 2],
+    alumniSuccessIds: [1, 2],
+    insfrastructureIds: [1, 2],
+
+    thumbnail:
+      "https://scontent.fpkr1-1.fna.fbcdn.net/v/t39.30808-1/294635551_582874376959305_9050633898427379300_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=104&cb=99be929b-8d691acd&ccb=1-7&_nc_sid=596444&_nc_eui2=AeHoBPgtsPk1r6K-lp-QkFVjgjhVLvgsxiWCOFUu-CzGJVkYJwZGJg7fBYJUvR9Wbmc-X4kGcZUzwu890JUzsyKU&_nc_ohc=UssbGqTSv9oAX_t0Tcl&_nc_oc=AQm7QKsynviMEE4U8rmNnM45GxfvS9ZR3s8haNZfkjU3ymPtL1yz3Df78YBLJWotGcMAqcjOLAJJB9Hk0cJQl5a9&_nc_ht=scontent.fpkr1-1.fna&oh=00_AfByLH6Deyd4Ayrc1Jdg45Hmzgk8oJ-R_yCnwABncyO05g&oe=65EB62CF",
+    description: "Fostering innovation and technology",
+    address: "Banepa,Nepal",
+    type: "Academy",
+    category: "Technology",
+    accreditation_status: "Accredited",
+    principal_name: "Shreeram kumar",
+    principal_email: "sophia@innovatorsacademy.edu",
+    principal_phone_number: "+9988776655",
+    files: "https://innovatorsacademy.edu/files/innovation_guide.pdf",
+    ownerId: 2,
+  },
+  {
+    name: "Nature's Conservatory",
+    albumIds: [1, 2],
+    programsIds: [1, 2],
+    facultyIds: [1, 2],
+    extraActivitiesIds: [1, 2],
+    costIds: [1, 2],
+    alumniSuccessIds: [1, 2],
+    insfrastructureIds: [1, 2],
+    thumbnail:
+      "https://ayurveda.com.np/wp-content/uploads/2023/07/ayurved-campust-visit.jpg.webp",
+    description: "Preserving and studying natural sciences",
+    address: "Dharan,Nepal",
+    type: "Conservatory",
+    category: "Natural Sciences",
+    accreditation_status: "Accredited",
+    principal_name: "Dr. Radha krishna koirala",
+    principal_email: "radha@naturesconservatory.edu",
+    principal_phone_number: "+9823334455",
+    files: "https://naturesconservatory.edu/files/research_papers.pdf",
+    ownerId: 2,
+  },
+  {
+    name: "Bindeswarri Montessori",
+    albumIds: [1, 2],
+    programsIds: [1, 2],
+    facultyIds: [1, 2],
+    extraActivitiesIds: [1, 2],
+    costIds: [1, 2],
+    alumniSuccessIds: [1, 2],
+    insfrastructureIds: [1, 2],
+    thumbnail:
+      "https://content.jdmagicbox.com/comp/chennai/30/044p5300730/catalogue/global-art-india-chennai-0zylxiwymx.jpg",
+    description: "Early education through Montessori method",
+    address: "Narayngarh,Chitwan",
+    type: "Preschool",
+    category: "Early Childhood Education",
+    accreditation_status: "Accredited",
+    principal_name: "Ms. Bindeswari Khadka",
+    principal_email: "bindeswari@montessori.edu",
+    principal_phone_number: "+95988776655",
+    files: "https://brightmindsmontessori.edu/files/parent_handbook.pdf",
+    ownerId: 1,
+  },
+  // Add more educational institution data as needed
+];
---
+++ groups.js
@@ -0,0 +1,99 @@
+export const groupData = [
+  {
+    name: "Nepali Sports Fanatics",
+    about: "Cheering for Nepali athletes and engaging in sports discussions",
+    adminUserIDs: [1],
+    memberUserIDs: [1, 5,4],
+  },
+  {
+    name: "Nepali Book Club",
+    about: "Delving into literature from Nepali authors and beyond",
+    adminUserIDs: [3],
+    memberUserIDs: [1, 3, 4, 8],
+  },
+ 
+  {
+    name: "Nepali Poetry Circle",
+    about: "Celebrating the richness of Nepali language through poetry",
+    adminUserIDs: [8],
+    memberUserIDs: [8, 9, 10],
+  },
+  {
+    name: "Kathmandu Cuisine Connoisseurs",
+    about: "Discovering and sharing the flavors of Nepali cuisine",
+    adminUserIDs: [10],
+    memberUserIDs: [5, 9, 10],
+  },
+
+  {
+    name: "Traditional Dance Troupe",
+    about: "Preserving and promoting Nepali traditional dances",
+    adminUserIDs: [9],
+    memberUserIDs: [5, 8, 9],
+  },
+  {
+    name: "Nepali History Society",
+    about: "Exploring the rich historical heritage of Nepal",
+    adminUserIDs: [7],
+    memberUserIDs: [7, 8, 10],
+  },
+  
+  {
+    
+    name: "Math Enthusiasts",
+    about: "A group for math lovers and problem solvers",
+    adminUserIDs: [1, 2],
+    memberUserIDs: [1, 2, 4],
+  },
+  {
+    name: "Science Explorers",
+    about: "Exploring the wonders of science together",
+    adminUserIDs: [2, 3],
+    memberUserIDs: [2, 3],
+  },
+  {
+    name: "Literature Lovers",
+    about: "Discussing classic and contemporary literature",
+    adminUserIDs: [1, 3],
+    memberUserIDs: [1, 3],
+  },
+  // Additional education-related group data
+  {
+    name: "History Buffs",
+    about: "Diving into historical events and discussions",
+    adminUserIDs: [4],
+    memberUserIDs: [1, 4],
+  },
+  {
+    name: "Programming Wizards",
+    about: "Mastering the art of coding and algorithms",
+    adminUserIDs: [6],
+    memberUserIDs: [6, 7],
+  },
+  {
+    name: "Art Aficionados",
+    about: "Appreciating various forms of art and creativity",
+    adminUserIDs: [2],
+    memberUserIDs: [1, 2, 4],
+  },
+  // Three more education-related groups
+  {
+    name: "Language Learners",
+    about: "Exploring new languages and cultures",
+    adminUserIDs: [7],
+    memberUserIDs: [3, 7, 4, 6],
+  },
+  {
+    name: "Environmental Scientists",
+    about: "Understanding the impact of environment on society",
+    adminUserIDs: [6],
+    memberUserIDs: [1, 6, 7],
+  },
+  {
+    name: "Music Enthusiasts",
+    about: "Sharing and discovering diverse music genres",
+    adminUserIDs: [3],
+    memberUserIDs: [3, 7],
+  },
+  // Add more education-related group data as needed
+];
